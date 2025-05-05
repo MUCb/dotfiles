b151aaa8d8c9db7ed39f0ccd2be020a39d28a28b
Vitalii Misko <vitalii.misko.eb@bp.renesas.com>
2025-05-05 12:21:59 +0300

    add git pull to startup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git pull origin main
+
 # Backup existing .bashrc if it exists
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.backup
 [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.backup
