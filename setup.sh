306fb6e78c535028128b3f602fcca36c34ab23aa
Vitalii Misko <vitalii.misko.eb@bp.renesas.com>
2025-05-05 14:32:34 +0300

    remove git pull

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-git pull origin main
-
 # Backup existing .bashrc if it exists
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.backup
 [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.backup
