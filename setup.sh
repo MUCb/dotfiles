e137d7a69c6f6605a7cdda0e7a19ce5192e241d7
Vitalii Misko <vitalii.misko.eb@bp.renesas.com>
2025-05-05 11:46:52 +0300

    add vimrc

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@
 
 # Backup existing .bashrc if it exists
 [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc.backup
+[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.backup
 
 stow bash --target=$HOME
+stow vim --target=$HOME
+
